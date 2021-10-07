383f99fb9ef692be2ec6cdcb153e8cc0d17b2f64
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-10-07 13:31:52 +0100

    add item.js

---
+++ src/items.js
@@ -0,0 +1,24 @@
+const items = {
+  A: {
+    price: 50,
+    offer: {
+      number: 3,
+      price: 130,
+    },
+  },
+  B: {
+    price: 30,
+    offer: {
+      number: 2,
+      price: 45,
+    },
+  },
+  C: {
+    price: 20,
+  },
+  D: {
+    price: 15,
+  },
+};
+
+export default items;
